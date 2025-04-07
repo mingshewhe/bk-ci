0598c580ff0d1964103ba2f4dd549efdda8c6f88
mingshewhe <mingshewhe@tencent.com>
2025-04-07 19:46:50 +0800

    feat:【PAC模板】流水线模板支持PAC特性 #11414
    # Conflicts:
    #	src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/constant/ProcessMessageCode.kt
    #	support-files/i18n/process/message_en_US.properties
    #	support-files/i18n/process/message_zh_CN.properties

--- src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/constant/ProcessMessageCode.kt
+++ src/backend/ci/core/process/api-process/src/main/kotlin/com/tencent/devops/process/constant/ProcessMessageCode.kt
@@ -607,4 +607,9 @@ object ProcessMessageCode {
     const val BK_YAML_PIPELINE_DELETE_VERSION_FAILED = "bkYamlPipelineDeleteVersionFailed"
     // [<a href={0}>{1}</a>]删除失败
     const val BK_YAML_PIPELINE_DELETE_FAILED = "bkYamlPipelineDeleteFailed"
+
+    // 自定义模板名称
+    const val BK_CUSTOM_PIPELINE_TEMPLATE_NAME = "bkCustomPipelineTemplateName"
+    // 研发商店模板名称
+    const val BK_MARKET_PIPELINE_TEMPLATE_NAME = "bkMarketPipelineTemplateName"
 }
