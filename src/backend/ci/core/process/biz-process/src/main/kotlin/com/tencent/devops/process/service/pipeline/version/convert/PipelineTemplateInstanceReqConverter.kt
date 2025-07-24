3f5e9988f0c075f1b4c209b80c285ed850e76f80
mingshewhe <mingshewhe@tencent.com>
2025-07-24 17:56:23 +0800

    feat:【PAC模板】流水线模板支持PAC特性 #11414 实例化增加覆盖模版字段

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/pipeline/version/convert/PipelineTemplateInstanceReqConverter.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/service/pipeline/version/convert/PipelineTemplateInstanceReqConverter.kt
@@ -61,6 +61,7 @@ import com.tencent.devops.process.service.template.v2.PipelineTemplateInstanceSe
 import com.tencent.devops.process.service.template.v2.PipelineTemplateResourceService
 import com.tencent.devops.process.service.template.v2.PipelineTemplateSettingService
 import com.tencent.devops.process.yaml.PipelineYamlService
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
@@ -183,6 +184,7 @@ class PipelineTemplateInstanceReqConverter(
 
             val overrideParamKeys = overrideTemplateField?.paramKeys
             val templateVariables = params?.filter {
+                logger.info("overrideParamKeys ${it.id}: ${overrideParamKeys?.contains(it.id) ?: false}")
                 overrideParamKeys?.contains(it.id) ?: false
             }?.map { TemplateVariable(it) }
 
@@ -328,5 +330,9 @@ class PipelineTemplateInstanceReqConverter(
             pipelineSetting.copy(pipelineAsCodeSettings = pipelineAsCodeSettings)
         } ?: pipelineSetting
         return pacSetting
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(PipelineTemplateInstanceReqConverter::class.java)
     }
 }
